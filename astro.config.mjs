4bc2856e77c3a6f7e5c6c5406dea89ca9cadf5b2
Hugo <hugomarcel91@gmail.com>
2023-09-19 22:40:12 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'hybrid',
-	adapter: node({
-		mode: "standalone"
-	}),
+	adapter: netlify(),
 	integrations: [svelte()],
 	redirects: {
 		"/dashboard": "/dashboard/eventos"
